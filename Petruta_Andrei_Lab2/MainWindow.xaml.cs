6e190472be38e53e7fec66710ca062f7c14a5b8f
Andrei Petruta <aandrei.petruta@gmail.com>
2020-12-23 11:21:21 +0200

    Setat comenzi predefinite

--- Petruta_Andrei_Lab2/MainWindow.xaml.cs
+++ Petruta_Andrei_Lab2/MainWindow.xaml.cs
@@ -48,6 +48,14 @@ namespace Petruta_Andrei_Lab2
         public MainWindow()
         {
             InitializeComponent();
+            //creare obiect binding pentru comanda
+            CommandBinding cmd1 = new CommandBinding();
+            //asociere comanda
+            cmd1.Command = ApplicationCommands.Print;
+            //asociem un handler
+            cmd1.Executed += new ExecutedRoutedEventHandler(CtrlP_CommandHandler);
+            //adaugam la colectia CommandBindings
+            this.CommandBindings.Add(cmd1);
         }
 
         private void glazedToolStripMenuItem_Click(object sender, RoutedEventArgs e)
@@ -242,6 +250,13 @@ namespace Petruta_Andrei_Lab2
                         break;
                 }
             }
+        }
+
+
+        private void CtrlP_CommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("You have in stock:" + mRaisedGlazed + " Glazed," + mRaisedSugar + "Sugar, "+mFilledLemon+" Lemon, "+mFilledChocolate+" Chocolate, "+mFilledVanilla+" Vanilla"
+           );
         }
 
     }
